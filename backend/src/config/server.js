a3e156c518a411fc906429478e7851920ead0c31
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-03-08 08:43:42 +0800

    Update server.js

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -6,13 +6,13 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      poolSize: 10, // maksimal 10 koneksi dalam pool
+      maxPoolSize: 10, // Ganti poolSize jadi maxPoolSize
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}✅`);
+    console.log(`MongoDB Connected: ${conn.connection.host} ✅`);
   } catch (error) {
-    console.log('MongoDB Connection Error ❌', error);
+    console.log("MongoDB Connection Error ❌", error);
     process.exit(1);
   }
 };
