20c634f4c034e99df638ad131ae365c4420e7a2a
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-03-10 22:45:04 +0800

    memisahkan getFunction Const ke helpers

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -11,10 +11,25 @@ export const getStatusBadgeClass = (status) => {
     return statusConfig[status] || "bg-gray-500";
   };
   
+  export const getStatusColorClass = (status) => {
+    switch (status) {
+      case "Belum Dikerjakan":
+        return "bg-red-500/20 text-red-400 border-red-600";
+      case "Sedang dikerjain...":
+        return "bg-blue-500/20 text-blue-400 border-blue-500";
+      case "Selesai":
+        return "bg-green-500/20 text-green-400 border-green-600";
+      case "Tertunda":
+        return "bg-purple-500/20 text-purple-400 border-purple-500";
+      default:
+        return "bg-gray-500/20 text-gray-400 border-gray-600";
+    }
+  };  
+
   export const getProgressColorClass = (progress) => {
-    if (progress < 30) return "bg-red-500";
-    if (progress < 70) return "bg-yellow-500";
-    return "bg-green-500";
+    if (progress >= 80) return "bg-emerald-500";
+    if (progress >= 50) return "bg-amber-500";
+    return "bg-rose-500";
   };
   
   export const getRemainingDays = (deadline) => {
@@ -25,3 +40,55 @@ export const getStatusBadgeClass = (status) => {
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
   };
   
+  export const mataKuliahOptions = [
+    {
+      value: "FTI2006 Etika Profesi",
+      label: "Etika Profesi",
+      icon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj8_3o93ELYEGvrnRZoe0pZa-Fwzs71vnSOeth5vyVkyMghqgmiq5pW4s49PwJxEtvJNUHC2MLPaElx2Daka9l_78puO9zNRZBYKt7syatoHanPqXpcL47lglnK2U1KkXFvEsBebzkHqobV/s700/ilustrasi+Kode+Etik+%2528sumber-+pelajaran.co.id%2529.jpg",
+    },
+    {
+      value: "MGU1007 Fiqih",
+      label: "Fiqih",
+      icon: "https://www.sinarbarualgensindo.com/wp-content/uploads/2020/06/IMG-20200612-WA0030.jpg",
+    },
+    {
+      value: "TIF3601 Jaringan Syaraf Tiruan",
+      label: "Jaringan Syaraf Tiruan",
+      icon: "https://www.unite.ai/wp-content/uploads/2019/08/artificial-neural-network-3501528_1280.png",
+    },
+    {
+      value: "TIF3602 Riset Operasi",
+      label: "Riset Operasi",
+      icon: "https://github.com/ahmad-syaifuddin/image-flow/blob/main/riset-operasi.png?raw=true",
+    },
+    {
+      value: "TIF3603 Sistem Penunjang Keputusan",
+      label: "Sistem Penunjang Keputusan",
+      icon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgOE-HwaJq1mw9ftv0ymXQtYgy68wXNA6VLIC6MrZ8YrF71EbOtGRwIRtO8V7RmHcZPZRPWoqCuI3ZQ6NDksSbyR8BOVjwFPiLsNoPQLYkHVvE8ihFDiRNRykyMvvf65X0BDD7c77jBq_w/s1600/Sistem+Pendukung+Keputusan+(SPK).jpg",
+    },
+    {
+      value: "TIF3604 Keamanan Sistem Komputer",
+      label: "Keamanan Sistem Komputer",
+      icon: "https://mohsai.com/wp-content/uploads/2023/07/Apa-itu-Keamanan-Jaringan-Macam-dan-Contohnya-keamanan-jariangan-adalah-1536x864.jpg",
+    },
+    {
+      value: "TIF3605 Manajemen Perangkat Lunak",
+      label: "Manajemen Perangkat Lunak",
+      icon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiVwLf7P1LyGO11oeNaIgTYEWdFg6piiykL_EnXIQMZhk-RkEDPH9LTML31EZRKJlbgyB1opgl-80KoToZMpZ-hj-6Mbk2C6eoyE3SYKzNDHyfRneofNw0nLcuxR9TxnH-kpgsTe0YwKYU/s320/Manajemen+Proyek+Konsep+1.jpg",
+    },
+    {
+      value: "TIF3606 Testing & Implementasi di Bidang TI",
+      label: "Testing & Implementasi di Bidang TI",
+      icon: "https://matheusrumetna.com/wp-content/uploads/2020/09/testing-sistem.png",
+    },
+    {
+      value: "TIF3607 Metodologi & Penelitian di Bidang TI",
+      label: "Metodologi & Penelitian di Bidang TI",
+      icon: "https://d1e4pidl3fu268.cloudfront.net/0d9f11d4-bfd3-4da5-9e47-2cf846004b6c/6676678542_quantitativedescriptiveresearchquantitativeresearchdesigncliparthd.crop_844x633_8,0.preview.png",
+    },
+    {
+      value: "TIF3609 Pengolahan Citra",
+      label: "Pengolahan Citra",
+      icon: "https://elearning2.be.bisa.ai/course/media/2022-08-14_140142_course.png",
+    },
+  ];
