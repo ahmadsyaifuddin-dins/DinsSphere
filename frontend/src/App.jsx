0107bd2aeb752a2d537ca396f4f243a222134f02
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-04-11 21:57:40 +0800

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -95,7 +95,7 @@ function App() {
         />
         <Route 
           path="/profile" 
-          element={user ? <Profile /> : <Navigate to="/login" />} 
+          element={<Profile />}  
         />
         <Route path="/login" element={<Login />} />
         <Route 
