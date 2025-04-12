69056259ef0a7026600756274b744027f836fdd6
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-04-12 16:01:40 +0800

    new music

--- frontend/src/data/tracks.js
+++ frontend/src/data/tracks.js
@@ -7,21 +7,26 @@ const tracks = [
     },
     { 
       id: 2, 
+      title: "Alphaville - Forever Young", 
+      src: "/Music/Alphaville - Forever Young.mp3" 
+    },
+    { 
+      id: 3, 
       title: "Hava Nagila Original", 
       src: "/Music/Hava Nagila Original.mp3" 
     },
     { 
-      id: 3, 
+      id: 4, 
       title: "Dance of the Sun and Fire 🔥", 
       src: "/Music/Nightcore - Dance of the Sun and Fire.mp3" 
     },
     { 
-      id: 4, 
+      id: 5, 
       title: "Hitler Speech - German people help yourself!", 
       src: "/Music/Hitler Speech German people help yourself!.mp3" 
     },
     { 
-      id: 5, 
+      id: 6, 
       title: "Chalino Sánchez - Alma Enamorada", 
       src: "/Music/Chalino Sánchez - Alma Enamorada.mp3" 
     },
