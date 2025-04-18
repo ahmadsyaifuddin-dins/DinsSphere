ac833367243b08f14783cdf3bd7aff7bbf02af84
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-04-18 21:28:30 +0800

    lupa jam dan minet

--- backend/src/messagesEmail/messageServiceTasks.js
+++ backend/src/messagesEmail/messageServiceTasks.js
@@ -33,6 +33,8 @@ function generateTaskHtml(task, action) {
         day: "numeric",
         month: "long",
         year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
       })
     : "-";
 
